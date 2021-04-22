e148816582527fd5e3ce4e1f0ba77fab9e52e702
Harsh Karande <harshskarande@gmail.com>
2021-04-22 15:38:36 +0530

    feat: add mdx support plugin for vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import mdx from "vite-plugin-mdx";
+
+const options = {
+  remarkPlugins: [],
+  rehypePlugins: [],
+};
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), mdx(options)],
 });
